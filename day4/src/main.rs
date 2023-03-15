85bd809b8596ac83400ae882e66a040b9e755c41
caplincapture <leuping@gmail.com>
2023-03-15 16:44:42 -0700

    4b master

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -20,7 +20,7 @@ fn main() {
                     d.parse::<u8>().unwrap(),
                 )
             })
-            .filter(|(a, b, c, d)| (a >= c && b <= d) || (a <= c && b >= d))
+            .filter(|(a, b, c, d)| a <= d && c <= b)
             .count()
     );
 
