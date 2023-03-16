ccef3039a1e9529299788bd1fa7049b4591f506f
caplincapture <leuping@gmail.com>
2023-03-16 13:47:15 -0700

    6b master

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,12 +1,20 @@
 
 
 fn main() {
-    println!(
-        "{}",
-        include_bytes!("../input.txt")
-            .windows(4)
-            .position(|b| !(0..3).any(|i| (i + 1..4).any(|j| b[i] == b[j])))
-            .unwrap()
-            + 4,
-    );
+    let mut i = 0;
+    while let Some(win) = include_bytes!("../input.txt").get(i..i + 14) {
+        let mut seen = 0u32;
+        if let Some(pos) = win.iter().rposition(|b| {
+            let bit = 1 << (b % 32);
+            let duplicate = seen & bit != 0;
+            seen |= bit;
+            duplicate
+        }) {
+            i += pos + 1;
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", i);
 }
