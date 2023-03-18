e047803a25ddc6108cb2508271a3cce3e32e76b8
caplincapture <leuping@gmail.com>
2023-03-18 09:39:35 -0700

    day 12b master

--- day12/day12master/src/main.rs
+++ day12/day12master/src/main.rs
@@ -1,5 +1,3 @@
-
-
 const NEXT: [(usize, usize); 4] = [(1, 0), (usize::MAX, 0), (0, 1), (0, usize::MAX)];
 
 pub fn main() {
@@ -18,19 +16,22 @@ pub fn main() {
 
     println!(
         "{}",
-        pathfinding::directed::bfs::bfs(
-            &(end % w, end / w),
-            |(x, y)| {
-                let cur = map[y * w + x];
-                NEXT.iter()
-                    .map(|(xx, yy)| (x.wrapping_add(*xx), y.wrapping_add(*yy)))
-                    .filter(|(x, y)| x < &w && y < &h && map[y * w + x] >= cur.saturating_sub(1))
-                    .collect::<Vec<_>>()
-            },
-            |&p| p == (start % w, start / w),
-        )
-        .unwrap()
-        .len()
-            - 1
+        map.iter()
+            .enumerate()
+            .filter(|(_, b)| **b == 0)
+            .filter_map(|(start, _)| pathfinding::directed::bfs::bfs(
+                &(start % w, start / w),
+                |(x, y)| {
+                    let cur = map[y * w + x];
+                    NEXT.iter()
+                        .map(|(xx, yy)| (x.wrapping_add(*xx), y.wrapping_add(*yy)))
+                        .filter(|(x, y)| x < &w && y < &h && map[y * w + x] <= cur + 1)
+                        .collect::<Vec<_>>()
+                },
+                |&p| p == (end % w, end / w),
+            )
+            .map(|r| r.len() - 1))
+            .min()
+            .unwrap()
     );
 }
