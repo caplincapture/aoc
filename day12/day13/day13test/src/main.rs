a1fb00158c4f85176fdf1f9066fec3c6dfab1874
caplincapture <leuping@gmail.com>
2023-03-18 10:07:37 -0700

    day 13b test

--- day12/day13/day13test/src/main.rs
+++ day12/day13/day13test/src/main.rs
@@ -24,17 +24,7 @@ impl std::cmp::PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match (self, other) {
             (Node::Number(a), Node::Number(b)) => a.partial_cmp(b),
-            (l, r) => Some(l.with_slice(|l| {
-                r.with_slice(|r| {
-                    l.iter()
-                        .zip(r.iter())
-                        .map(|(aa, bb)| aa.cmp(bb))
-                        // return the first ordering that isn't `Equal`
-                        .find(|&ord| ord != Ordering::Equal)
-                        // or compare the lengths
-                        .unwrap_or_else(|| l.len().cmp(&r.len()))
-                })
-            })),
+            (l, r) => l.with_slice(|l| r.with_slice(|r| l.partial_cmp(r))),
         }
     }
 }
@@ -55,22 +45,24 @@ impl fmt::Debug for Node {
 }
 
 fn main() {
-    let mut sum = 0;
-    for (i, groups) in include_str!("../input.txt").split("\n\n").enumerate() {
-        let i = i + 1;
+    let dividers = vec![
+        Node::List(vec![Node::Number(2)]),
+        Node::List(vec![Node::Number(6)]),
+    ];
 
-        let mut nodes = groups
-            .lines()
-            .map(|line| serde_json::from_str::<Node>(line).unwrap());
-        let l = nodes.next().unwrap();
-        let r = nodes.next().unwrap();
-        println!("\n== Pair {i} ==");
-        println!("l = {l:?}");
-        println!("r = {r:?}");
-        println!("l < r = {}", l < r);
-        if l < r {
-            sum += i;
-        }
-    }
-    dbg!(sum);
+    let mut packets = include_str!("../input.txt")
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|line| serde_json::from_str::<Node>(line).unwrap())
+        .chain(dividers.iter().cloned())
+        .collect::<Vec<_>>();
+
+    packets.sort();
+
+    let decoder_key = dividers
+        .iter()
+        .map(|d| packets.binary_search(d).unwrap() + 1)
+        .product::<usize>();
+
+    dbg!(decoder_key);
 }
